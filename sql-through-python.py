2a7cee6c983abaa45806f179c36520a4954ff9b1
breogann <fernandocostapradillo@gmail.com>
2023-02-02 09:52:01 +0100

    uploading

--- sql-through-python.py
+++ sql-through-python.py
@@ -1,2 +1,8 @@
-def connection_sql():
-    return connection
+import os
+from dotenv import load_dotenv
+
+password = os.getenv("password")
+
+def password_printer():
+    return f"My password is {password}"
+
